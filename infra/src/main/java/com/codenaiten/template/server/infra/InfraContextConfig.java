20c929516323cff6b4008702add60a8af2d0e22b
Ángel Herce Soto <indenaiten@gmail.com>
2026-02-11 21:42:05 +0100

    init: Project initialized

---
+++ infra/src/main/java/com/codenaiten/template/server/infra/InfraContextConfig.java
@@ -0,0 +1,12 @@
+package com.codenaiten.template.server.infra;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan( basePackages = "com.codenaiten.template.server.infra" )
+public class InfraContextConfig {
+
+}
+
+
