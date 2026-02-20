29ea58bf93ae5ad452f8157d5cef1b7083bfd330
Ángel Herce Soto <indenaiten@gmail.com>
2026-02-20 15:02:37 +0100

    work 9

--- web/src/main/java/com/codenaiten/template/server/web/rest/feature/user/response/UserPublicInfoResponse.java
+++ web/src/main/java/com/codenaiten/template/server/web/rest/feature/user/response/UserPublicInfoResponse.java
@@ -3,11 +3,12 @@ package com.codenaiten.template.server.web.rest.feature.user.response;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record UserPublicInfoResponse( UUID id,
-                                      String username,
-                                      String name,
-                                      String surname,
-                                      LocalDateTime createdAt
+public record UserPublicInfoResponse(
+        UUID id,
+        String username,
+        String name,
+        String surname,
+        LocalDateTime createdAt
 ) {
 }
 
